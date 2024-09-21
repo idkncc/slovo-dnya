d6ef06d0f6d3df7881ce9c86ec247a1c936535dd
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2024-09-21 19:33:18 +0300

    feat: first version

---
+++ index.js
@@ -0,0 +1,52 @@
+require("dotenv/config")
+
+const fs = require("fs");
+const axios = require("axios");
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+const FILENAME = process.env.FILENAME ?? "words.txt";
+
+function publishWord(word) {
+    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`
+
+    axios(url, {
+        method: "POST",
+        data: {
+            chat_id: Number.isNaN(parseInt(CHAT_ID)) ? CHAT_ID : parseInt(CHAT_ID),
+            text: [
+                "*Слово дня 😋🤔🫡*",
+                "",
+                word
+            ].join("\n"),
+            parse_mode: "MarkdownV2"
+        },
+    })
+        .then(() => {
+            console.info("Published word of a day!")
+        })
+        .catch((err) => {
+            console.error("Couldn't publish word of a day...")
+            console.error(err.response.data)
+            process.exit(1)
+        })
+}
+
+function main() {
+    let output = "";
+
+    const readStream = fs.createReadStream(FILENAME);
+
+    readStream.on('data', (chunk) => {
+        output += chunk.toString('utf8');
+    });
+
+    readStream.on('end', () => {
+        const lines = output.split("\n");
+        const index = Math.floor(Math.random() * lines.length);
+
+        publishWord(lines[index]);
+    });
+}
+
+main();
